b6c742338c74fe7b82d95c120a12bc50ad2599a8
Andy Casey <andycasey@gmail.com>
2015-03-17 12:10:35 +0000

    Added conversion script from 2dfdr -> standardised FITS format

---
+++ convert/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Convert reduced frames into the standard GALAH FITS format. """
+
+__author__ = "Andy Casey <arc@ast.cam.ac.uk>"
+
+from .tdfdr import from_2dfdr
---
+++ convert/tdfdr.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+""" Convert 2dfdr reduced images to extracted spectra in GALAH FITS format. """
+
+__author__ = "Andy Casey <arc@ast.cam.ac.uk>"
+
+__all__ = ["from_2dfdr"]
+
+from subprocess import check_output
+
+import numpy as np
+
+from astropy.io import fits
+from astropy.constants import c as speed_of_light
+import motions
+
+# [TODO] Read the GAP version from elsewhere.
+GAP_VER = 6.4
+GAP_GIT_HASH = check_output("git rev-parse --short HEAD".split()).strip()
+
+
+def verify_hermes_origin(image):
+    assert image[0].header["ORIGIN"].strip() == "AAO"
+    assert image[0].header["INSTRUME"].strip() == "HERMES-2dF"
+
+
+def read_2dfdr_extensions(image):
+    """
+    Parse the image extensions from a 2dfdr-reduced image.
+
+    :param image:
+        The 2dfdr-reduced image.
+
+    :type image:
+        :class:`astropy.io.fits.hdu.hdulist.HDUList`
+    """
+
+    extensions = {
+        "data": 0,
+    }
+    extnames = map(str.strip, [hdu.header.get("EXTNAME", "") for hdu in image])
+    extensions["variance"] = extnames.index("VARIANCE")
+    extensions["fibres"] = extnames.index("FIBRES")
+    return extensions
+
+
+def from_2dfdr(reduced_filename):
+    """
+    Returns a list of `astropy.io.fits.hdu.hdulist.HDUList` objects
+    (1 HDU List per 2dfdr program object).
+
+    :param reduced_filename:
+        The path of the 2dfdr-reduced combined filename.
+
+    :type reduced_filename:
+        str
+    """
+
+    image = fits.open(reduced_filename)
+    ext = read_2dfdr_extensions(image)
+
+    # Verifications
+    verify_hermes_origin(image)
+
+    # Header modifications.
+    keywords = ["NAME", "RA", "DEC", "PMRA", "PMDEC", ("MAGNITUDE", "MAG"),
+        ("COMMENT", "DESCR")]
+    keyword_comments = {
+        "NAME": "Input source name",
+        "DESCR": "Input source comment",
+        "MAG": "Input source magnitude",
+        "RA": "Right Ascension (degrees)",
+        "DEC": "Declination (degrees)",
+        "PMRA": "Proper motion in RA (mas/yr)",
+        "PMDEC": "Proper motion in DEC (mas/yr)",
+        "FIBRE": "Fibre number"
+    }
+    header_template = image[ext["data"]].header.copy()
+    for column in ("CDELT2", "CRPIX2", "CRVAL2", "CTYPE2", "CUNIT2"):
+        del header_template[column]
+
+    header_template["GAP_VER"] = GAP_VER
+    header_template["GAP_HASH"] = GAP_GIT_HASH
+    header_template.comments["GAP_VER"] = "GALAH Analysis Pipeline version"
+    header_template.comments["GAP_HASH"] = "GALAH Analysis Pipeline commit hash"
+
+    extracted_sources = []
+    for program_index in np.where(image[ext["fibres"]].data["TYPE"] == "P")[0]:
+
+        flux = image[ext["data"]].data[program_index, :]
+        variance = image[ext["variance"]].data[program_index, :]
+        header = header_template.copy()
+
+        # Add header information from the fibre table.
+        header["FIBRE"] = program_index + 1
+        fibre_header = image[ext["fibres"]].data[program_index]
+        for keyword in keywords:
+            if isinstance(keyword, (str, unicode)):
+                header[keyword] = fibre_header[keyword]
+            else:
+                from_keyword, to_keyword = keyword
+                header[to_keyword] = fibre_header[from_keyword]
+
+        # The 2dfdr fibre table has the RA and DEC in radians. Who does that?!
+        if "RA" in keywords and "DEC" in keywords:
+            header["RA"] *= 180./np.pi
+            header["DEC"] *= 180./np.pi
+
+        # Add associated comments for the fibre table information
+        for keyword, comment in keyword_comments.items():
+            header.comments[keyword] = comment
+
+        # Create the HDUList.
+        hdu_flux = fits.PrimaryHDU(data=flux, header=header,
+            do_not_scale_image_data=True)
+        hdu_flux.header["EXTNAME"] = "input_spectrum"
+        hdu_flux.header.comments["EXTNAME"] = "Rebinned spectrum"
+
+        hdu_sigma = fits.ImageHDU(data=variance**0.5, header=header,
+            do_not_scale_image_data=True)
+        hdu_sigma.header["EXTNAME"] = "input_sigma"
+        hdu_sigma.header.comments["EXTNAME"] = "Rebinned sigma"
+
+        # Calculate the barycentric motion.
+        v_bary, v_helio = motions.from_header(header)
+
+        # Add motion correction information.
+        for hdu in (hdu_flux, hdu_sigma):
+            hdu.header["CRVAL1"] *= 1. + (v_bary/speed_of_light).value
+            hdu.header["CDELT1"] *= 1. + (v_bary/speed_of_light).value
+
+            hdu.header["V_BARY"] = v_bary.to("km/s").value
+            hdu.header["V_HELIO"] = v_helio.to("km/s").value
+            hdu.header.comments["V_BARY"] = "Barycentric motion (km/s)"
+            hdu.header.comments["V_HELIO"] = "Heliocentric motion (km/s)"
+            hdu.header["HISTORY"] = "Corrected for barycentric motion (V_BARY)"
+
+        # Create HDUList and add checksums.
+        hdulist = fits.HDUList([hdu_flux, hdu_sigma])
+        [hdu.add_checksum() for hdu in hdulist]
+        extracted_sources.append(hdulist)
+
+    image.close()
+
+    return extracted_sources
+
+
+
