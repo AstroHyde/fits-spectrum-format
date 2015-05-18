519ef3539c3fbec17b5f166a7ee223da34be23c8
Andy Casey <andycasey@gmail.com>
2015-05-19 00:21:12 +0100

    minor changes

--- run_gap_v6.2_standardisation.py
+++ run_gap_v6.2_standardisation.py
@@ -19,9 +19,9 @@ for i, folder in enumerate(folders):
 
     for ccd in range(1, 5):
         print("\tChecking CCD {}".format(ccd))
-        fco_folders = sorted(glob("{0}ccd_{1}/".format(folder, ccd)))
+        fco_folders = sorted(glob("{0}ccd_{1}/*/".format(folder, ccd)))
 
-        for fco, fco_folder in enumerate(fco_folders):
+        for fco, fco_folder in enumerate(fco_folders, start=1):
 
             fits_filename = glob("{0}/*_combined.fits".format(fco_folder))
             
@@ -40,7 +40,9 @@ for i, folder in enumerate(folders):
                     #(YYMMDD*1000+night_fco_id)
                     date = int(spectrum[0].header["FILEORIG"].split("/")[4])
                     name = spectrum[0].header.get("NAME", None)
-                    if name is None or not name.lower().startswith("galahic_"):
+
+                    if isinstance(name, (str, unicode)) \
+                    and name.lower().startswith("galahic_"):
                         name = name.split("_")[1]
                     else:
                         name = "FIB{0}".format(spectrum[0].header["FIBRE"])
